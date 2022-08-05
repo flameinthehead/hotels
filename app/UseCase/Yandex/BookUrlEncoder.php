5d4d7f0c2e202470467bfe83f60a79c229463ce5
Oleg Shmakov <o.shmakov@zaymigo.com>
2022-08-06 01:14:03 +0300

    Фикс.

--- app/UseCase/Yandex/BookUrlEncoder.php
+++ app/UseCase/Yandex/BookUrlEncoder.php
@@ -6,7 +6,7 @@ use App\UseCase\Search\BookUrlEncoderInterface;
 
 class BookUrlEncoder implements BookUrlEncoderInterface
 {
-    public const PARTNER_LINK_PATTERN = 'https://tp.media/r?marker=193372&trs=179356&p=5916&u={ищщл}&campaign_id=193';
+    public const PARTNER_LINK_PATTERN = 'https://tp.media/r?marker=193372&trs=179356&p=5916&u={book_url}&campaign_id=193';
 
     public function encode(string $bookLink): string
     {
