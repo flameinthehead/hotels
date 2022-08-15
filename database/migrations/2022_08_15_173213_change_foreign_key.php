d42cc542d29498f6125a06fc2c6ee872cb76cc68
Oleg Shmakov <o.shmakov@zaymigo.com>
2022-08-15 21:43:23 +0300

    Фикс.

--- database/migrations/2022_08_15_173213_change_foreign_key.php
+++ database/migrations/2022_08_15_173213_change_foreign_key.php
@@ -18,11 +18,11 @@ return new class extends Migration
         });
 
         Schema::table('telegram_requests', function (Blueprint $table) {
-            $table->foreignId('city_id')->constrained('cities', 'city_id')->onDelete('CASCADE');
-            /*$table->foreign('city_id')
+//            $table->foreignId('city_id')->constrained('cities', 'city_id')->onDelete('CASCADE');
+            $table->foreign('city_id')
                 ->references('city_id')
                 ->on('cities')
-                ->onDelete('cascade');*/
+                ->onDelete('cascade');
         });
     }
 
